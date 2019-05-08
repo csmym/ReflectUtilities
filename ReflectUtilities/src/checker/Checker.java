5ba811fc4eefbaa556db87c7b04662f6fca95dce
Yvan <yvan.maillot@gmail.com>
2019-05-09 00:54:11 +0200

    Réglage d'un petit bug
    IntrospectionUtilities est remplacé par ReflectUtilities suite à un oubli après un changement de nom.

--- ReflectUtilities/src/checker/Checker.java
+++ ReflectUtilities/src/checker/Checker.java
@@ -6,25 +6,24 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Formatter;
+import java.util.List;
+import java.util.stream.Collectors;
 import tags.CheckModifier;
 import tags.GetterToCheck;
 import tags.SetterToCheck;
 import tags.ToCompare;
-import tests.A;
 
 /**
  *
  * @author yvan
  */
-@ToCheck("Class")
 public class Checker {
 
-    @ToCheck("Attribut")
     private StringBuilder SB;
     private Formatter F;
+    // La classe testée.
     private final Class<?> C;
 
-    @ToCheck("Constructeur")
     public Checker(Class<?> c) {
         SB = new StringBuilder();
         F = new Formatter(SB);
@@ -32,7 +31,6 @@ public class Checker {
         check();
     }
 
-    @ToCheck("Méthode")
     private void f(String format, Object... o) {
         F.format(format, o);
     }
@@ -58,6 +56,13 @@ public class Checker {
         testMethods();
     }
 
+    /**
+     * Écrit le test unitaire dont le but est de vérifier 
+     *      - l'exactitude du nom de cette classe,
+     *      - de ses modificateurs,
+     *      - de sa super classe.
+     * @param toCheck l'annotation qui annote cette classe.
+     */
     public void checkClass(ToCheck toCheck) {
         f("\n@Test\n");
         f("public void p%06d000_checkClass%s() {\n", toCheck.priority(), C.getSimpleName());
@@ -68,13 +73,15 @@ public class Checker {
                 C.getSuperclass().getSimpleName());
         f("   Class<?> x = %s.class;\n", C.getSimpleName());
         checkModifiers(toCheck.value(), C.getModifiers(), toCheck.modifiers());
-        /*f("   assertTrue(\"%s\", %s.class.getModifiers() == %s);\n",
-                toCheck.value() + " (Modificateurs)",
-                C.getSimpleName(),
-                C.getModifiers()
-        );*/
-
+        /* #### TRAVAUX EN COURS #### */// checkImplementations(toCheck);
         f("}\n");
+    }
+    /* #### TRAVAUX EN COURS #### */
+    public void checkImplementations(ToCheck toCheck) {
+        List<Class<?>> implementations = Arrays.stream(C.getInterfaces())
+                .sorted((i1, i2) -> i1.getName().compareTo(i2.getName()))
+                .collect(Collectors.toList());
+        f("   List<Class<?>> implementations = Arrays.stream(x.getInterfaces())\n");
     }
 
     private void checkConstructor(Constructor<?> c, ToCheck toCheck) {
@@ -170,7 +177,7 @@ public class Checker {
         f("   for (int i = 0; i < 100; ++i) {\n");
         f("      try {\n");
         f("         StringBuilder msg = new StringBuilder(\"revoir %s.%s() --> \");\n", C.getSimpleName(), m.getName());
-        f("         boolean result = IntrospectionUtilities.sameResult(msg, classRef, classToTest, \"%s\");\n", m.getName());
+        f("         boolean result = ReflectUtilities.sameResult(msg, classRef, classToTest, \"%s\");\n", m.getName());
         f("         assertTrue(msg.toString(), result);\n");
         f("      } catch (IllegalArgumentException | InvocationTargetException | NoSuchMethodException | NoSuchFieldException | IllegalAccessException | InstantiationException ex) {\n");        
         f("         fail(\"revoir %s.%s()\");\n", C.getSimpleName(), m.getName());
@@ -190,10 +197,15 @@ public class Checker {
 
     }
 
+    /**
+     * Vérifie le champ f de la classe testée
+     * @param f
+     * @param toCheck 
+     */
     private void checkField(Field f, ToCheck toCheck) {
         f("\n@Test\n");
 
-        f("public void p%06d000_checkField%s%s() {\n", toCheck.priority(), C.getSimpleName(), f.getName());
+        f("public void p%06d000_check%sField%s() {\n", toCheck.priority(), C.getSimpleName(), initial(f.getName()));
         f("   System.out.println(\"Check attribut %s\");\n", f.getName());
         f("   try {\n");
         f("        Field x = %s.class.getDeclaredField(\"%s\");\n", C.getSimpleName(), f.getName());
@@ -227,12 +239,12 @@ public class Checker {
                 f.getType().getSimpleName());
         f("        assertTrue(\"Revoir set%s\", x.getReturnType().equals(void.class));\n",
                 initial(f.getName()));
-        f("        Object o = IntrospectionUtilities.randomValue(%s.class);\n",
+        f("        Object o = ReflectUtilities.randomValue(%s.class);\n",
                 C.getSimpleName());
         f("        for(int i = 0; i < 100; ++i) {\n");
-        f("            %1$s v = (%1$s) IntrospectionUtilities.randomValue(%1$s.class);\n",
+        f("            %1$s v = (%1$s) ReflectUtilities.randomValue(%1$s.class);\n",
                 f.getType().getSimpleName());
-        f("            IntrospectionUtilities.getFromMethodTA(\n");
+        f("            ReflectUtilities.getFromMethodTA(\n");
         f("                 %s.class,\n", C.getSimpleName());
         f("                 o,\n");
         f("                 \"set%s\",\n", initial(f.getName()));
@@ -240,7 +252,7 @@ public class Checker {
         f("                 v\n");
         f("            );\n");
         f("            assertTrue(\"Revoir set%s\", ", initial(f.getName()));
-        f("                                        IntrospectionUtilities.getAttribut(\n");
+        f("                                        ReflectUtilities.getAttribut(\n");
         f("                                           %s.class, \n", C.getSimpleName());
         f("                                           o, \n");
         f("                                           \"%s\"\n", f.getName());
@@ -268,10 +280,10 @@ public class Checker {
     private void checkGetter(Field f, GetterToCheck toCheck) {
         /*           
             for (int i = 0; i < 100; ++i) {
-                Object o = IntrospectionUtilities.randomValue(Point.class);
-                Object attr = IntrospectionUtilities.getAttribut(Point.class, o, "x");
-                Object getAttr = IntrospectionUtilities.getFromMethod(Point.class, o, "getX");
-                assertTrue("Revoir getX", IntrospectionUtilities.equals(attr, getAttr));
+                Object o = ReflectUtilities.randomValue(Point.class);
+                Object attr = ReflectUtilities.getAttribut(Point.class, o, "x");
+                Object getAttr = ReflectUtilities.getFromMethod(Point.class, o, "getX");
+                assertTrue("Revoir getX", ReflectUtilities.equals(attr, getAttr));
             }
         } catch (SecurityException | NoSuchMethodException | NoSuchFieldException ex) {
             fail("Revoir getX");
@@ -288,10 +300,10 @@ public class Checker {
         f("   System.out.println(\"Check getter %s\");\n", attributName);
         f("   try {\n");
         f("     for(int i = 0; i < 100; ++i) {\n");
-        f("         Object o = IntrospectionUtilities.randomValue(%s); \n", className);
-        f("         Object attr = IntrospectionUtilities.getAttribut(%s, o, \"%s\");\n", className, attributName);
-        f("         Object getAttr = IntrospectionUtilities.getFromMethod(%s, o, \"%s\");\n", className, getterName);
-        f("         assertTrue(\"Revoir %s\", IntrospectionUtilities.equals(attr, getAttr));\n", getterName);
+        f("         Object o = ReflectUtilities.randomValue(%s); \n", className);
+        f("         Object attr = ReflectUtilities.getAttribut(%s, o, \"%s\");\n", className, attributName);
+        f("         Object getAttr = ReflectUtilities.getFromMethod(%s, o, \"%s\");\n", className, getterName);
+        f("         assertTrue(\"Revoir %s\", ReflectUtilities.equals(attr, getAttr));\n", getterName);
         f("     }\n");
         f("   } catch (SecurityException | NoSuchMethodException | NoSuchFieldException ex) {\n");
         f("        fail(\"Revoir %s\");\n", getterName);
